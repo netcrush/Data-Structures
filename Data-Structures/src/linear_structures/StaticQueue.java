6d820353d39c15a79dc661b7b4a31b3ebf290dce
netcrush <netcrush@gmail.com>
2014-03-04 21:44:27 +0100

    StaticQueue changed. New methods added: - isEmpty() - isFull() - enqueue(int data)

--- Data-Structures/src/linear_structures/StaticQueue.java
+++ Data-Structures/src/linear_structures/StaticQueue.java
@@ -5,4 +5,26 @@ public class StaticQueue {
 	private int numberOfElements;
 	private int first;
 	private int last;
+	
+	public StaticQueue(int kapacity) {
+		array = new int[kapacity];
+		numberOfElements = 0;
+		first = -1;
+		last = -1;
+	}
+	
+	private boolean isEmpty() {
+		return (numberOfElements == 0);
+	}
+	
+	private boolean isFull() {
+		return (numberOfElements == array.length);
+	}
+	
+	public boolean enqueue(int data) {
+		if (isFull()) return false;
+		array[last = (++last % array.length)] = data;
+		numberOfElements++;
+		return true;
+	}
 }
